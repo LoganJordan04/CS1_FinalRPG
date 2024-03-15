c3649971f8a2d460a1dfb44e9503f85e477d0b22
Logan <ljordan@cocc.edu>
2024-03-15 16:15:10 -0700

    Better game loop and random enemy weapon.

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import os
+import random
 from characters import Hero, Enemy
-from weapons import short_bow, iron_sword
+from weapons import iron_sword, weapon_choices
 
 
 def main():
     name = input("Enter the hero's name: ")
     hero = Hero(name, 100)
     hero.equip(iron_sword)
-    enemy = Enemy("Enemy", 100, short_bow)
 
-    while True:
+    random_weapon = random.choice(weapon_choices)
+    enemy = Enemy("Enemy", 100, random_weapon)
+
+    while hero.health > 0 and enemy.health > 0:
         # Clears the screen if ran in terminal.
         os.system("cls")
 
         hero.attack(enemy)
         enemy.attack(hero)
-
         hero.health_bar.draw()
         enemy.health_bar.draw()
 
-        if not input("Press Enter to quit: "):
-            break
+        if hero.health <= 50:
+            hero.heal()
+            hero.health_bar.draw()
+            enemy.health_bar.draw()
+
+        input()
 
 
 if __name__ == "__main__":

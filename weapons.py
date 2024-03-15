beae82ca671548f54766de0ebb3f6a1f6a37771e
Logan <ljordan@cocc.edu>
2024-03-15 16:14:14 -0700

    Weapon choices added.

--- weapons.py
+++ weapons.py
@@ -8,3 +8,5 @@ class Weapon:
 iron_sword = Weapon("Iron Sword", "melee", 5)
 short_bow = Weapon("Short Bow", "ranged", 4)
 fists = Weapon("Fists", "melee", 2)
+
+weapon_choices = [iron_sword, short_bow, fists]

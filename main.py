8195991f2856c0a5bf88c5c6d4206bd0558c1361
Logan <ljordan@cocc.edu>
2024-03-20 11:08:59 -0700

    Player sprites.

--- main.py
+++ main.py
@@ -55,18 +55,18 @@ class Hero(Character):
         return f"{self.name} equipped {self.weapon.name}!"
 
     def heal(self):
-        if self.health >= self.health_max - 25:
+        if self.health >= self.health_max - 50:
             self.heal_amount = self.health_max - self.health
         else:
-            self.heal_amount = 25
+            self.heal_amount = 50
         self.health += self.heal_amount
         self.health_bar.update()
 
     def heal_amount_update(self):
-        if self.health >= self.health_max - 25:
+        if self.health >= self.health_max - 50:
             self.heal_amount = self.health_max - self.health
         else:
-            self.heal_amount = 25
+            self.heal_amount = 50
 
     def heal_print(self):
         if self.heal_amount == 0:
@@ -74,22 +74,90 @@ class Hero(Character):
         else:
             print(f"{self.name} healed {self.heal_amount} health!")
 
-    def draw(self, weapon):
-        self.weapon = weapon
-        # Original pixel art from La3eb https://opengameart.org/forumtopic/how-to-get-better-at-P-art
-        # Hex to ANSI color converter https://ajalt.github.io/colormath/converter/
-        # Sets the cursor position.
-        term.pos(1, 7)
-        # Player sprite art.
-        print(f"{P(251, 251).c} {P(0, 244).c}{P(244, 244).c}{P(244, 244).c}{P(0, 244).c}")
-        term.pos(2, 7)
-        print(f"{P(251, 180).c}{P(0, 31).c}{P(233, 180).c}{P(180, 180).c}{P(233, 180).c}{P(180, 94).c}"
-              f"{P(0, 94).c}{P(0, 94).c}")
-        term.pos(3, 7)
-        print(f"{P(31, 0).c}{P(31, 0).c}{P(31, 31).c}{P(31, 31).c}{P(31, 31).c}{P(94, 94).c}{P(237, 94).c}"
-              f"{P(94, 94).c}")
-        term.pos(4, 7)
-        print(f"  {P(244, 244).c}{P(244, 0).c}{P(244, 0).c}{P(244, 244).c}")
+    # Original chicken player pixel art by handsofhope
+    # https://www.reddit.com/r/PixelArt/comments/fqu1ri/wanted_to_take_a_stab_at_restricting_size_8x8_and/
+    def draw(self):
+        # Player sprite art when no weapon is equipped.
+        if self.weapon == fists:
+            # Hex to ANSI color converter https://ajalt.github.io/colormath/converter/
+            # Sets the cursor position.
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(0, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c}")
+        # Player sprite art when dagger is equipped.
+        elif self.weapon == dagger:
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c} {P(0, 185).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(0, 231).c}{P(185, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c}")
+        # Player sprite art when iron sword is equipped.
+        elif self.weapon == iron_sword:
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c} {P(251, 251).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(0, 231).c}{P(251, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c} {P(238, 0).c}")
+        # Player sprite art when iron axe is equipped.
+        elif self.weapon == iron_axe:
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c}{P(0, 244).c}{P(0, 251).c}{P(0, 251).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c}{P(244, 244).c}"
+                  f"{P(251, 0).c}{P(251, 251).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(244, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c}{P(238, 0).c}")
+        # Player sprite art when staff is equipped.
+        elif self.weapon == staff:
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c} {P(125, 251).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c} {P(251, 251).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(0, 231).c}{P(251, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c} {P(251, 251).c}")
+        # Player sprite art when iron greatsword is equipped.
+        elif self.weapon == iron_greatsword:
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c} {P(231, 231).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c} {P(231, 231).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(0, 231).c}{P(251, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c} {P(238, 0).c}")
+        # Player sprite art when iron battleaxe is equipped.
+        elif self.weapon == iron_battleaxe:
+            term.pos(2, 7)
+            print(f"    {P(0, 231).c}{P(0, 231).c}{P(244, 244).c}{P(231, 231).c}{P(231, 231).c}")
+            term.pos(3, 7)
+            print(f" {P(0, 231).c}{P(231, 231).c}{P(231, 233).c}{P(231, 231).c}{P(231, 233).c}{P(244, 244).c}"
+                  f" {P(231, 0).c}")
+            term.pos(4, 7)
+            print(f"{P(0, 231).c}{P(231, 231).c}{P(231, 231).c}{P(231, 231).c}{P(215, 231).c}{P(215, 203).c}"
+                  f"{P(244, 231).c}")
+            term.pos(5, 7)
+            print(f" {P(231, 215).c}{P(231, 0).c}{P(231, 0).c}{P(231, 0).c}{P(231, 215).c}{P(244, 244).c}")
 
 
 class Enemy(Character):
@@ -99,121 +167,122 @@ class Enemy(Character):
         self.health_bar = HealthBar(self, color="red")
         self.tier = tier
 
+    # Original enemy pixel art by La3eb https://opengameart.org/forumtopic/how-to-get-better-at-P-art
     def draw(self):
         # Slime sprite art.
         if self.name == "Slime":
-            term.pos(1, 37)
-            print(f" {P(0, 83).c}{P(83, 34).c}{P(83, 34).c}{P(83, 34).c}{P(0, 22).c} ")
             term.pos(2, 37)
-            print(f"{P(0, 83).c}{P(83, 34).c}{P(34, 34).c}{P(83, 34).c}{P(83, 83).c}{P(22, 34).c}{P(0, 22).c}")
+            print(f" {P(0, 83).c}{P(83, 34).c}{P(83, 34).c}{P(83, 34).c}{P(0, 22).c} ")
             term.pos(3, 37)
-            print(f"{P(83, 83).c}{P(34, 34).c}{P(34, 34).c}{P(34, 34).c}{P(34, 34).c}{P(34, 22).c}{P(22, 22).c}")
+            print(f"{P(0, 83).c}{P(83, 34).c}{P(34, 34).c}{P(83, 34).c}{P(83, 83).c}{P(22, 34).c}{P(0, 22).c}")
             term.pos(4, 37)
+            print(f"{P(83, 83).c}{P(34, 34).c}{P(34, 34).c}{P(34, 34).c}{P(34, 34).c}{P(34, 22).c}{P(22, 22).c}")
+            term.pos(5, 37)
             print(f" {P(22, 0).c}{P(22, 0).c}{P(22, 0).c}{P(22, 0).c}{P(22, 0).c}")
         # Rat sprite art.
         elif self.name == "Rat":
-            term.pos(2, 37)
-            print(f" {P(251, 243).c}{P(251, 243).c}{P(0, 243).c}{P(251, 243).c}{P(251, 251).c}")
             term.pos(3, 37)
+            print(f" {P(251, 243).c}{P(251, 243).c}{P(0, 243).c}{P(251, 243).c}{P(251, 251).c}")
+            term.pos(4, 37)
             print(f"{P(0, 243).c}{P(167, 243).c}{P(243, 243).c}{P(167, 243).c}{P(243, 243).c}{P(0, 243).c}"
                   f" {P(251, 251).c}")
-            term.pos(4, 37)
+            term.pos(5, 37)
             print(f" {P(251, 0).c} {P(243, 243).c}{P(243, 243).c}{P(243, 243).c}{P(243, 243).c}")
         # Goblin sprite art.
         elif self.name == "Goblin":
-            term.pos(1, 37)
-            print(f"  {P(0, 34).c}{P(34, 34).c}{P(34, 34).c}{P(0, 34).c}")
             term.pos(2, 37)
-            print(f"{P(0, 34).c} {P(185, 34).c}{P(34, 34).c}{P(185, 34).c}{P(34, 22).c}")
+            print(f"  {P(0, 34).c}{P(34, 34).c}{P(34, 34).c}{P(0, 34).c}")
             term.pos(3, 37)
-            print(f"{P(34, 0).c}{P(22, 0).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 34).c}{P(22, 34).c}")
+            print(f"{P(0, 34).c} {P(185, 34).c}{P(34, 34).c}{P(185, 34).c}{P(34, 22).c}")
             term.pos(4, 37)
+            print(f"{P(34, 0).c}{P(22, 0).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 34).c}{P(22, 34).c}")
+            term.pos(5, 37)
             print(f"  {P(22, 22).c}{P(22, 0).c}{P(22, 0).c}{P(22, 22).c}")
         # Goblin sprite art.
         elif self.name == "Skeleton":
-            term.pos(1, 37)
-            print(f" {P(0, 251).c}{P(251, 251).c}{P(251, 251).c}{P(0, 251).c}")
             term.pos(2, 37)
-            print(f" {P(233, 251).c}{P(251, 251).c}{P(233, 251).c}{P(251, 240).c}{P(0, 240).c}{P(0, 251).c}")
+            print(f" {P(0, 251).c}{P(251, 251).c}{P(251, 251).c}{P(0, 251).c}")
             term.pos(3, 37)
-            print(f"{P(251, 0).c} {P(240, 0).c}{P(240, 251).c}{P(240, 251).c}{P(240, 0).c} {P(251, 0).c}")
+            print(f" {P(233, 251).c}{P(251, 251).c}{P(233, 251).c}{P(251, 240).c}{P(0, 240).c}{P(0, 251).c}")
             term.pos(4, 37)
+            print(f"{P(251, 0).c} {P(240, 0).c}{P(240, 251).c}{P(240, 251).c}{P(240, 0).c} {P(251, 0).c}")
+            term.pos(5, 37)
             print(f"  {P(240, 240).c}{P(240, 0).c}{P(240, 0).c}{P(240, 240).c}")
         # Goblin Thief sprite art.
         elif self.name == "Goblin Thief":
-            term.pos(1, 37)
-            print(f"{P(0, 185).c} {P(0, 244).c}{P(244, 244).c}{P(244, 244).c}{P(0, 244).c}")
             term.pos(2, 37)
-            print(f"{P(185, 34).c}{P(0, 22).c}{P(185, 34).c}{P(34, 34).c}{P(185, 34).c}{P(34, 22).c}{P(0, 22).c}")
+            print(f"{P(0, 185).c} {P(0, 244).c}{P(244, 244).c}{P(244, 244).c}{P(0, 244).c}")
             term.pos(3, 37)
-            print(f"{P(22, 0).c}{P(22, 0).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 34).c}{P(22, 22).c}")
+            print(f"{P(185, 34).c}{P(0, 22).c}{P(185, 34).c}{P(34, 34).c}{P(185, 34).c}{P(34, 22).c}{P(0, 22).c}")
             term.pos(4, 37)
+            print(f"{P(22, 0).c}{P(22, 0).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 34).c}{P(22, 22).c}")
+            term.pos(5, 37)
             print(f"  {P(244, 244).c}{P(244, 0).c}{P(244, 0).c}{P(244, 244).c}")
         # Sword Warrior sprite art.
         elif self.name == "Sword Warrior":
-            term.pos(1, 37)
-            print(f"{P(251, 251).c} {P(0, 244).c}{P(244, 244).c}{P(244, 244).c}{P(0, 244).c}")
             term.pos(2, 37)
+            print(f"{P(251, 251).c} {P(0, 244).c}{P(244, 244).c}{P(244, 244).c}{P(0, 244).c}")
+            term.pos(3, 37)
             print(f"{P(251, 180).c}{P(0, 31).c}{P(233, 180).c}{P(180, 180).c}{P(233, 180).c}{P(180, 94).c}"
                   f"{P(0, 94).c}{P(0, 94).c}")
-            term.pos(3, 37)
+            term.pos(4, 37)
             print(f"{P(31, 0).c}{P(31, 0).c}{P(31, 31).c}{P(31, 31).c}{P(31, 31).c}{P(94, 94).c}{P(237, 94).c}"
                   f"{P(94, 94).c}")
-            term.pos(4, 37)
+            term.pos(5, 37)
             print(f"  {P(244, 244).c}{P(244, 0).c}{P(244, 0).c}{P(244, 244).c}")
         # Axe Warrior sprite art.
         elif self.name == "Axe Warrior":
-            term.pos(1, 37)
+            term.pos(2, 37)
             print(f"{P(251, 251).c}{P(251, 251).c}{P(244, 244).c}{P(0, 251).c}{P(251, 251).c}{P(251, 251).c}"
                   f"{P(0, 251).c}")
-            term.pos(2, 37)
+            term.pos(3, 37)
             print(f"{P(251, 0).c} {P(244, 244).c}{P(233, 180).c}{P(180, 180).c}{P(233, 180).c}{P(180, 31).c}"
                   f"{P(0, 31).c}")
-            term.pos(3, 37)
-            print(f"  {P(180, 0).c}{P(31, 31).c}{P(31, 31).c}{P(31, 31).c}{P(31, 31).c}{P(31, 0).c}")
             term.pos(4, 37)
+            print(f"  {P(180, 0).c}{P(31, 31).c}{P(31, 31).c}{P(31, 31).c}{P(31, 31).c}{P(31, 0).c}")
+            term.pos(5, 37)
             print(f"   {P(251, 251).c}{P(251, 0).c}{P(251, 0).c}{P(251, 251).c}")
         # Goblin Mage sprite art.
         elif self.name == "Goblin Mage":
-            term.pos(1, 37)
-            print(f"{P(125, 251).c} {P(0, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 0).c}")
             term.pos(2, 37)
-            print(f"{P(251, 251).c} {P(185, 34).c}{P(34, 34).c}{P(185, 34).c}{P(22, 22).c} {P(0, 34).c}")
+            print(f"{P(125, 251).c} {P(0, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 0).c}")
             term.pos(3, 37)
-            print(f"{P(34, 251).c}{P(22, 0).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 0).c}")
+            print(f"{P(251, 251).c} {P(185, 34).c}{P(34, 34).c}{P(185, 34).c}{P(22, 22).c} {P(0, 34).c}")
             term.pos(4, 37)
+            print(f"{P(34, 251).c}{P(22, 0).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 0).c}")
+            term.pos(5, 37)
             print(f"{P(251, 251).c} {P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(22, 22).c}{P(0, 22).c}")
         # Undead Mage sprite art.
         elif self.name == "Undead Mage":
-            term.pos(1, 37)
-            print(f"{P(125, 251).c} {P(0, 251).c}{P(251, 251).c}{P(251, 251).c}{P(0, 251).c}")
             term.pos(2, 37)
+            print(f"{P(125, 251).c} {P(0, 251).c}{P(251, 251).c}{P(251, 251).c}{P(0, 251).c}")
+            term.pos(3, 37)
             print(f"{P(251, 92).c}{P(0, 92).c}{P(233, 251).c}{P(251, 251).c}{P(233, 251).c}{P(251, 92).c}{P(0, 92).c}"
                   f"{P(251, 0).c}")
-            term.pos(3, 37)
-            print(f"{P(251, 251).c} {P(92, 92).c}{P(92, 92).c}{P(92, 92).c}{P(92, 92).c}")
             term.pos(4, 37)
+            print(f"{P(251, 251).c} {P(92, 92).c}{P(92, 92).c}{P(92, 92).c}{P(92, 92).c}")
+            term.pos(5, 37)
             print(f"{P(251, 251).c} {P(92, 92).c}{P(92, 92).c}{P(92, 92).c}{P(92, 92).c}{P(92, 92).c}{P(0, 92).c}")
         # Sword Knight sprite art.
         elif self.name == "Sword Knight":
-            term.pos(1, 37)
-            print(f"{P(244, 244).c} {P(244, 251).c}{P(0, 251).c}{P(0, 251).c}{P(244, 251).c}")
             term.pos(2, 37)
-            print(f"{P(244, 244).c} {P(233, 251).c}{P(233, 233).c}{P(233, 251).c}{P(251, 251).c}")
+            print(f"{P(231, 231).c} {P(244, 251).c}{P(0, 251).c}{P(0, 251).c}{P(244, 251).c}")
             term.pos(3, 37)
-            print(f"{P(251, 0).c}{P(251, 0).c}{P(251, 251).c}{P(251, 251).c}{P(244, 244).c}{P(244, 246).c}"
-                  f"{P(244, 244).c}")
+            print(f"{P(231, 231).c} {P(233, 251).c}{P(233, 233).c}{P(233, 251).c}{P(251, 251).c}")
             term.pos(4, 37)
+            print(f"{P(251, 238).c}{P(251, 0).c}{P(251, 251).c}{P(251, 251).c}{P(244, 244).c}{P(244, 246).c}"
+                  f"{P(244, 244).c}")
+            term.pos(5, 37)
             print(f"  {P(251, 251).c}{P(251, 0).c}{P(244, 0).c}{P(244, 251).c}{P(244, 0).c}")
         # Axe Knight sprite art.
         elif self.name == "Axe Knight":
-            term.pos(1, 37)
-            print(f"{P(251, 251).c}{P(251, 251).c}{P(244, 244).c}{P(0, 251).c}{P(0, 251).c}{P(0, 251).c}{P(0, 251).c}")
             term.pos(2, 37)
-            print(f"{P(251, 0).c} {P(244, 244).c}{P(233, 251).c}{P(233, 233).c}{P(233, 251).c}{P(251, 251).c}")
+            print(f"{P(231, 231).c}{P(231, 231).c}{P(244, 244).c}{P(0, 251).c}{P(0, 251).c}{P(0, 251).c}{P(0, 251).c}")
             term.pos(3, 37)
-            print(f"  {P(244, 251).c}{P(251, 251).c}{P(251, 251).c}{P(251, 251).c}{P(251, 251).c}{P(251, 251).c}")
+            print(f"{P(231, 0).c} {P(244, 244).c}{P(233, 251).c}{P(233, 233).c}{P(233, 251).c}{P(251, 251).c}")
             term.pos(4, 37)
+            print(f"  {P(244, 251).c}{P(251, 251).c}{P(251, 251).c}{P(251, 251).c}{P(251, 251).c}{P(251, 251).c}")
+            term.pos(5, 37)
             print(f"  {P(244, 244).c}{P(251, 251).c}{P(251, 0).c}{P(251, 0).c}{P(251, 251).c}")
 
 
@@ -255,9 +324,9 @@ class HealthBar:
                   f"{self.colors["default"] if self.is_colored else ""}"
                   f"{self.barrier}")
         else:
-            term.pos(6, 30)
-            print(f"{self.entity.name}'s health: {self.entity.health}/{self.entity.health_max}")
             term.pos(7, 30)
+            print(f"{self.entity.name}'s health: {self.entity.health}/{self.entity.health_max}")
+            term.pos(8, 30)
             print(f"{self.barrier}"
                   f"{self.color if self.is_colored else ""}"
                   f"{remaining_bars * self.symbol_remaining}"
@@ -284,8 +353,8 @@ skeleton = Enemy("Skeleton", 60, fists, 1)
 goblin_thief = Enemy("Goblin Thief", 110, dagger, 2)
 sword_warrior = Enemy("Sword Warrior", 130, iron_sword, 2)
 axe_warrior = Enemy("Axe Warrior", 120, iron_axe, 2)
-goblin_mage = Enemy("Goblin Mage", 70, staff, 3)
-undead_mage = Enemy("Undead Mage", 80, staff, 3)
+goblin_mage = Enemy("Goblin Mage", 90, staff, 3)
+undead_mage = Enemy("Undead Mage", 100, staff, 3)
 sword_knight = Enemy("Sword Knight", 180, iron_greatsword, 4)
 axe_knight = Enemy("Axe Knight", 170, iron_battleaxe, 4)
 
@@ -299,16 +368,18 @@ def main():
     # Clears the screen if ran in terminal.
     os.system("cls")
 
-    hero.equip(iron_sword)
-
     while True:
         enemy = random.choice(enemy_list)
-        Hero.draw(hero, iron_sword)
+        Hero.draw(hero)
         Enemy.draw(enemy)
         hero.health_bar.draw()
         enemy.health_bar.draw()
         print("\n+-------------------------------------------------+")
-        print(f"{hero.name} encountered a(n) {enemy.name}!")
+        # Statements for "a" vs "an" in encounter text.
+        if enemy.name.lower().startswith(('a', 'e', 'i', 'o', 'u')):
+            print(f"{hero.name} encountered an {enemy.name}!")
+        else:
+            print(f"{hero.name} encountered a {enemy.name}!")
         print(f"\nPress A to attack.\nPress H to heal {hero.heal_amount} health.")
 
         while hero.health > 0 and enemy.health > 0:
@@ -317,7 +388,7 @@ def main():
                 hero.attack(enemy)
                 enemy.attack(hero)
 
-                Hero.draw(hero, iron_sword)
+                Hero.draw(hero)
                 Enemy.draw(enemy)
                 hero.health_bar.draw()
                 enemy.health_bar.draw()
@@ -332,7 +403,7 @@ def main():
                 os.system("cls")
                 hero.heal()
 
-                Hero.draw(hero, iron_sword)
+                Hero.draw(hero)
                 Enemy.draw(enemy)
                 hero.health_bar.draw()
                 enemy.health_bar.draw()

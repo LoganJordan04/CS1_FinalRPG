33515fbbd6bd94ea29d270466d442234c8a06cb8
Logan <ljordan@cocc.edu>
2024-03-20 12:04:06 -0700

    Modularized again.

---
+++ healthbar.py
@@ -0,0 +1,44 @@
+import term
+
+
+class HealthBar:
+    symbol_remaining = "█"
+    symbol_lost = "_"
+    barrier = "|"
+    colors = {"default": "\033[0m",
+              "green": "\033[92m",
+              "red": "\033[91m",
+              }
+
+    def __init__(self, entity, length=20, is_colored=True, color=""):
+        self.entity = entity
+        self.length = length
+        self.max_value = entity.health_max
+        self.current_value = entity.health
+        self.is_colored = is_colored
+        self.color = self.colors.get(color) or self.colors["default"]
+
+    def update(self):
+        self.current_value = self.entity.health
+
+    def draw(self):
+        remaining_bars = round(self.current_value / self.max_value * self.length)
+        lost_bars = self.length - remaining_bars
+        if self.entity.name == "You":
+            print(f"\n{self.entity.name}r health: {self.entity.health}/{self.entity.health_max}")
+            print(f"{self.barrier}"
+                  f"{self.color if self.is_colored else ""}"
+                  f"{remaining_bars * self.symbol_remaining}"
+                  f"{lost_bars * self.symbol_lost}"
+                  f"{self.colors["default"] if self.is_colored else ""}"
+                  f"{self.barrier}")
+        else:
+            term.pos(7, 30)
+            print(f"{self.entity.name}'s health: {self.entity.health}/{self.entity.health_max}")
+            term.pos(8, 30)
+            print(f"{self.barrier}"
+                  f"{self.color if self.is_colored else ""}"
+                  f"{remaining_bars * self.symbol_remaining}"
+                  f"{lost_bars * self.symbol_lost}"
+                  f"{self.colors["default"] if self.is_colored else ""}"
+                  f"{self.barrier}")
---
+++ weapons.py
@@ -0,0 +1,15 @@
+class Weapon:
+    def __init__(self, name, damage):
+        self.name = name
+        self.damage = damage
+
+
+# Weapons
+fists = Weapon("Fists", 3)
+claws = Weapon("Claws", 4)
+dagger = Weapon("Dagger", 5)
+iron_sword = Weapon("Iron Sword", 6)
+iron_axe = Weapon("Iron Axe", 8)
+staff = Weapon("Staff", 10)
+iron_greatsword = Weapon("Iron Greatsword", 12)
+iron_battleaxe = Weapon("Iron Battleaxe", 14)

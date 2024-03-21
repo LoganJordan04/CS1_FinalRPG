62155ddf58c79f72447977838a1bd3f08b1438a9
Logan <ljordan@cocc.edu>
2024-03-21 12:02:07 -0700

    Complete battle and advance screen rewrite.

--- main.py
+++ main.py
@@ -9,15 +9,95 @@ from characters import *
 os.system("")
 
 
+def battle_print(hero, enemy, start):
+    if start:
+        # Clears the screen
+        os.system("cls")
+        Hero.draw(hero)
+        Enemy.draw(enemy)
+        hero.health_bar.draw()
+        enemy.health_bar.draw()
+        print("\n+-------------------------------------------------+")
+        # Statements for "a" vs "an" in encounter text.
+        if enemy.name.lower().startswith(('a', 'e', 'i', 'o', 'u')):
+            print(f"{hero.name} encountered an {enemy.name}!")
+        else:
+            print(f"{hero.name} encountered a {enemy.name}!")
+        # Potion amount print.
+        print(f"\nYou have {hero.potions} potions.")
+        # Text to advance turn.
+        if hero.potions > 0:
+            print(f"\nPress A to attack.\nPress H to heal {hero.heal_amount} health.")
+        else:
+            print(f"\nPress A to attack.")
+    else:
+        os.system("cls")
+        Hero.draw(hero)
+        Enemy.draw(enemy)
+        hero.health_bar.draw()
+        enemy.health_bar.draw()
+        print("\n+-------------------------------------------------+")
+        hero.attack_print(enemy)
+        enemy.attack_print(hero)
+        # Updates the healing amount.
+        hero.heal_update()
+        print(f"\nYou have {hero.potions} potions.")
+        # Determines if the player can heal or not.
+        if hero.potions > 0:
+            print(f"\nPress A to attack.\nPress H to heal {hero.heal_amount} health.")
+        else:
+            print(f"\nPress A to attack.")
+
+
+def drop_print(hero, enemy):
+    print(f"\n{enemy.name} dropped {enemy.weapon.name}! Press E to equip it.")
+    # Comparing the current weapon and dropped weapon.
+    print(f"{hero.weapon.name} damage: {hero.weapon.damage} -> "
+          f"{enemy.weapon.name} damage: {enemy.weapon.damage}")
+
+
+def advance_print(hero, enemy, is_collected, is_dropped, is_buying):
+    if not is_buying:
+        os.system("cls")
+        Hero.draw(hero)
+        hero.health_bar.draw()
+        print("\n+-------------------------------------------------+")
+        # If the dropped coins aren't collected yet.
+        if not is_collected:
+            hero.coins += enemy.coins
+            print(f"{enemy.name} dropped {enemy.coins} coins!")
+            print(f"\nYou now have {hero.coins} coins and {hero.potions} potions")
+        else:
+            print(f"You have {hero.coins} coins and {hero.potions} potions")
+        # If the enemy dropped their weapon.
+        if is_dropped:
+            drop_print(hero, enemy)
+        print(f"\nPress B to buy a potion for 20 coins.\nPress Enter to advance.")
+
+    else:
+        os.system("cls")
+        Hero.draw(hero)
+        hero.health_bar.draw()
+        print("\n+-------------------------------------------------+")
+        if hero.coins < 20:
+            print("You don't have enough to buy a potion!")
+            print(f"\nYou have {hero.coins} coins and {hero.potions} potions")
+        # Buying a potion.
+        else:
+            hero.potions += 1
+            hero.coins -= 20
+            print(f"You now have {hero.coins} coins and {hero.potions} potions")
+        if is_dropped:
+            drop_print(hero, enemy)
+        print(f"\nPress B to buy a potion for 20 coins.\nPress Enter to advance.")
+
+
 def main():
     hero = Hero("You", 150, 5, 0)
 
     enemy_num = 1
 
     while True:
-        # Clears the screen.
-        os.system("cls")
-
         # Picking a random enemy of the first tier.
         if enemy_num <= 5:
             enemy = random.choice(tier1_enemies)
@@ -44,111 +124,61 @@ def main():
         enemy.reset_health()
         enemy.health_bar.update()
 
-        Hero.draw(hero)
-        Enemy.draw(enemy)
-        hero.health_bar.draw()
-        enemy.health_bar.draw()
-        print("\n+-------------------------------------------------+")
-        # Statements for "a" vs "an" in encounter text.
-        if enemy.name.lower().startswith(('a', 'e', 'i', 'o', 'u')):
-            print(f"{hero.name} encountered an {enemy.name}!")
-        else:
-            print(f"{hero.name} encountered a {enemy.name}!")
-        # Potion amount print.
-        print(f"\nYou have {hero.potions} potions.")
-        if hero.potions > 0:
-            print(f"\nPress A to attack.\nPress H to heal {hero.heal_amount} health.")
-        else:
-            print(f"\nPress A to attack.")
+        # Initial battle screen printing.
+        battle_print(hero, enemy, start=True)
 
         while hero.health > 0 and enemy.health > 0:
             if keyboard.is_pressed("a"):
-                os.system("cls")
                 hero.attack(enemy)
                 enemy.attack(hero)
-
-                Hero.draw(hero)
-                Enemy.draw(enemy)
-                hero.health_bar.draw()
-                enemy.health_bar.draw()
-                print("\n+-------------------------------------------------+")
-                hero.attack_print(enemy)
-                enemy.attack_print(hero)
-                hero.heal_update()
-                print(f"\nYou have {hero.potions} potions.")
-                if hero.potions > 0:
-                    print(f"\nPress A to attack.\nPress H to heal {hero.heal_amount} health.")
-                else:
-                    print(f"\nPress A to attack.")
+                battle_print(hero, enemy, start=False)
                 # Delay to fix problems with rapid inputs.
                 time.sleep(0.25)
             elif keyboard.is_pressed("h"):
-                os.system("cls")
                 hero.heal()
-
-                Hero.draw(hero)
-                Enemy.draw(enemy)
-                hero.health_bar.draw()
-                enemy.health_bar.draw()
-                print("\n+-------------------------------------------------+")
-                hero.heal_print()
-                hero.heal_update()
-                print(f"\nYou have {hero.potions} potions.")
-                if hero.potions > 0:
-                    print(f"\nPress A to attack.\nPress H to heal {hero.heal_amount} health.")
-                else:
-                    print(f"\nPress A to attack.")
+                battle_print(hero, enemy, start=False)
                 time.sleep(0.25)
 
         if hero.health == 0:
             os.system("cls")
             return False
 
-        os.system("cls")
-        Hero.draw(hero)
-        hero.health_bar.draw()
-        print("\n+-------------------------------------------------+")
-        hero.coins += enemy.coins
-        print(f"{enemy.name} dropped {enemy.coins} coins! You now have {hero.coins} coins.")
+        # This sets the value for the coin drop text to be printed.
+        is_collected = False
 
         # 1/2 chance for an enemy to drop their weapon.
         item_drop = random.randint(1, 2)
+
         # Weapon can't be equipped if its fists, teeth, or hero is already holding it.
         if (item_drop == 1 and enemy.weapon.name != "Fists" and enemy.weapon.name != "Teeth"
                 and enemy.weapon.name != hero.weapon.name):
-            print(f"\n{enemy.name} also dropped {enemy.weapon.name}! Press E to equip it.\n"
-                  f"Press B to buy a potion for 20 coins.\nPress Enter to advance.")
+            is_dropped = True
+            advance_print(hero, enemy, is_collected, is_dropped, is_buying=False)
+            # Collecting the coin drop.
+            is_collected = True
             while keyboard.read_key() != "enter":
                 # Equipping the dropped weapon.
-                if keyboard.read_key() == "e":
+                if keyboard.read_key() == "e" and enemy.weapon.name != hero.weapon.name:
+                    is_dropped = False
                     hero.equip(enemy.weapon)
+                    advance_print(hero, enemy, is_collected, is_dropped, is_buying=False)
                     time.sleep(0.25)
                 # Buying a potion if the player has sufficient coins.
-                elif keyboard.read_key() == "b":
-                    if hero.coins >= 20:
-                        hero.potions += 1
-                        hero.coins -= 20
-                        print(f"\nYou now have {hero.potions} potions.")
-                        time.sleep(0.25)
-                    else:
-                        print("\nYou don't have enough to buy a potion!")
-                        time.sleep(0.25)
+                if keyboard.read_key() == "b":
+                    advance_print(hero, enemy, is_collected, is_dropped, is_buying=True)
+                    time.sleep(0.25)
                 # Hitting enter breaks the loop to advance the game.
                 elif keyboard.read_key() == "enter":
                     break
         # If the player didn't get a weapon drop.
         else:
-            print(f"\nPress B to buy a potion for 20 coins.\nPress Enter to advance.")
+            is_dropped = False
+            advance_print(hero, enemy, is_collected, is_dropped, is_buying=False)
+            is_collected = True
             while keyboard.read_key() != "enter":
                 if keyboard.read_key() == "b":
-                    if hero.coins >= 20:
-                        hero.potions += 1
-                        hero.coins -= 20
-                        print(f"\nYou now have {hero.potions} potions.")
-                        time.sleep(0.25)
-                    else:
-                        print("\nYou don't have enough to buy a potion!")
-                        time.sleep(0.25)
+                    advance_print(hero, enemy, is_collected, is_dropped, is_buying=True)
+                    time.sleep(0.25)
                 elif keyboard.read_key() == "enter":
                     break
 
